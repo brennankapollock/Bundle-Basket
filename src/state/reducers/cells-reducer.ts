15a9d23e28104bad33dc22db0d22cfa1b95a0305
brennankapollock <brennankapollock@gmail.com>
2022-05-02 08:33:55 -0500

    added cell shifting logic

--- src/state/reducers/cells-reducer.ts
+++ src/state/reducers/cells-reducer.ts
@@ -22,9 +22,22 @@ const initialState: CellState = {
 const reducer = produce((state: CellState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      return;
     case ActionType.DELETE_CELL:
-      return state;
+      delete state.data[action.payload];
+      state.order = state.order.filter((cellId) => cellId !== action.payload);
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     case ActionType.UPDATE_CELL:
